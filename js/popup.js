a9d689e42883c8e2cc6195559d9c032912f92b7a
!sh <developerish@users.noreply.github.com>
2025-05-17 22:47:17 -0700

    Migrate extension to Manifest V3

--- js/popup.js
+++ js/popup.js
@@ -1,71 +1,57 @@
-//get count of tabs in current window. Required for the popoup display box.
-function getCurrentWindowTabCount() {
-  chrome.tabs.query({currentWindow:true}, function(tabs) {
-    lenText = 'Number of tabs on this window:<strong> ' + tabs.length + '</strong>';
-    document.getElementById('windowTabs').innerHTML = lenText;
-  });
+"use strict";
+
+async function getCurrentWindowTabCount() {
+  const tabs = await chrome.tabs.query({ currentWindow: true });
+  const lenText = `Number of tabs on this window:<strong> ${tabs.length}</strong>`;
+  document.getElementById('windowTabs').innerHTML = lenText;
 }
 
-//get tabs in current window
-function getCurrentWindowTabs(callback) {
-  chrome.tabs.query({currentWindow:true}, function(tabs) {
-    callback(tabs);
-  });
+async function getCurrentWindowTabs() {
+  return await chrome.tabs.query({ currentWindow: true });
 }
 
-//get all tabs
-function getAllTabs(callback) {
-  chrome.tabs.query({}, function(tabs) {
-    callback(tabs);
-  });
+async function getAllTabs() {
+  return await chrome.tabs.query({});
 }
 
-function displayResults(tabs){
-  getCurrentWindowTabCount();
-  numTabs = tabs.length;
-  var table = document.getElementById('tabsTable');
-  for (var i=0; i<numTabs; i++) {
-    var row = table.insertRow(i);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-    
-    cell1.innerHTML = "<img src=" + tabs[i].favIconUrl + " width='16' height='16'>";
+async function displayResults(tabs) {
+  await getCurrentWindowTabCount();
+  const table = document.getElementById('tabsTable');
+  for (const tab of tabs) {
+    const row = table.insertRow(-1);
+    const cell1 = row.insertCell(0);
+    const cell2 = row.insertCell(1);
+    const cell3 = row.insertCell(2);
+
+    if (tab.favIconUrl) {
+      cell1.innerHTML = `<img src=${tab.favIconUrl} width='16' height='16'>`;
+    }
     cell2.innerHTML = "<span style=cursor:pointer><font color=red>X</font></span>";
-    cell3.innerHTML = "<span style=cursor:pointer title='" + tabs[i].url + "'>" +  tabs[i].title + "</span>";
+    cell3.innerHTML = `<span style=cursor:pointer title='${tab.url}'>${tab.title}</span>`;
 
-    cell2.addEventListener("click", (function(tabID) {
-      return function() {
-        closeTab(tabID);
-      }
-    })(tabs[i].id));
-    
-    cell3.addEventListener("click", (function(tabID, windowID) {
-      return function() {
-        openTab(tabID, windowID);
-      }
-    })(tabs[i].id, tabs[i].windowId));
+    cell2.addEventListener('click', () => closeTab(tab.id));
+    cell3.addEventListener('click', () => openTab(tab.id, tab.windowId));
   }
 }
 
-// function to display the selected tab
 function openTab(tabID, windowID) {
-  chrome.windows.update(windowID, {focused:true});
-  chrome.tabs.update(tabID, {active:true});
+  chrome.windows.update(windowID, { focused: true });
+  chrome.tabs.update(tabID, { active: true });
 }
 
-// function to close the selected tab
 function closeTab(tabID) {
   chrome.tabs.remove(tabID);
-  // reload popup to refresh the count and links
   window.location.reload();
 }
 
-var tabsDisplayOption = localStorage["popupDisplayOption"];
-// if extension is just installed or reloaded, tabsDisplayOption will not be set
-if (typeof tabsDisplayOption == "undefined" || tabsDisplayOption == "currentWindow") {
-  getCurrentWindowTabs(displayResults);
-} else {
-  //getCurrentWindowTabCount();
-  getAllTabs(displayResults);
-}
+(async () => {
+  const { popupDisplayOption } = await chrome.storage.local.get('popupDisplayOption');
+  if (popupDisplayOption === undefined || popupDisplayOption === 'currentWindow') {
+    const tabs = await getCurrentWindowTabs();
+    displayResults(tabs);
+  } else {
+    const tabs = await getAllTabs();
+    displayResults(tabs);
+  }
+})();
+
